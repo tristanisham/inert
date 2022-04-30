fa7a3b249aa24a011b56b40f516ffbbfc3546579
Tristan Isham <tristan@Ham-GamingPC.localdomain>
2022-04-30 11:17:35 -0400

    recursive works

--- src/builder/html.rs
+++ src/builder/html.rs
@@ -1,6 +1,6 @@
 use async_std::{fs, path::Path};
 
 
-pub async fn html<P: AsRef<Path>>(path: &P, entry: &fs::DirEntry) -> Result<(), String> {
+pub async fn html<P: AsRef<Path>>(_path: &P, _entry: &fs::DirEntry) -> Result<(), String> {
     Ok(())
 }
--- src/cmd.rs
+++ src/cmd.rs
@@ -1,6 +1,7 @@
 use crate::builder;
 use async_std::path::PathBuf;
-use async_std::{fs, future::Future, path::Path, prelude::*};
+use async_std::{fs, path::Path, prelude::*};
+use futures::future::{BoxFuture, FutureExt};
 use std::env;
 
 // pub fn init() -> Result<(), String> {
@@ -28,59 +29,59 @@ pub fn version() {
 /// `build()` generates inert static files in CONFIG:default_dir
 ///
 /// *standard default dir is* `_site`
-pub async fn build(target: Option<&str>) -> Result<(), String> {
-    builder_holder(target).await;
-
+pub async fn build<'a>(target: Option<&str>) -> Result<(), String> {
+    async_builder(target).await;
     Ok(())
 }
-/// `build_holder` is a static container function that facilitates the recursive nature of 
+/// `build_holder` is a static container function that facilitates the recursive nature of
 /// [async_builder()](#cmd.async_builder)
-pub fn builder_holder<'a>(target: std::option::Option<&'a str>) -> impl Future<Output = ()> + 'a {
-    async_builder(target)
-}
 
 /// async_builder recursivily scans through the child documents of the provided path, calling itself if it detects a directory
 /// this design is to facilitate a 1:1 directory structure copying into the **default** site directory.
-async fn async_builder(target: Option<&str>) {
-    let current_dir: PathBuf = match env::current_dir() {
-        Ok(dir) => PathBuf::from(dir),
-        Err(_) => panic!("init failed. Current grab current directory."),
-    };
-    let target_dir = match target {
-        Some(s) => PathBuf::from(s),
-        None => current_dir.clone(),
-    };
-
-    let cwd = match current_dir.to_str() {
-        Some(s) => s,
-        None => ".",
-    };
-
-    let curpath = format!("{0}/_site", cwd);
-    if !Path::new(&curpath).exists().await {
-        fs::create_dir("_site").await.unwrap();
-    }
-
-    if let Ok(mut entries) = fs::read_dir(target_dir).await {
-        while let Some(entry) = entries.next().await {
-            if let Ok(entry) = entry {
-                if let Ok(file_type) = entry.file_type().await {
-                    if file_type.is_dir() {
-                        let new_path = &entry.path();
-                        if let Some(np) = new_path.to_str() {
-                            build(Some(np));
+fn async_builder<'a>(target: Option<&str>) -> BoxFuture<()> {
+    async move {
+        let current_dir: PathBuf = match env::current_dir() {
+            Ok(dir) => PathBuf::from(dir),
+            Err(_) => panic!("init failed. Current grab current directory."),
+        };
+        let target_dir = match target {
+            Some(s) => PathBuf::from(s),
+            None => current_dir.clone(),
+        };
+        let cwd = match current_dir.to_str() {
+            Some(s) => s,
+            None => ".",
+        };
+        let curpath = format!("{0}/_site", cwd);
+        if !Path::new(&curpath).exists().await {
+            fs::create_dir("_site").await.unwrap();
+        }
+        if let Ok(mut entries) = fs::read_dir(target_dir).await {
+            while let Some(entry) = entries.next().await {
+                if let Ok(entry) = entry {
+                    if let Ok(file_type) = entry.file_type().await {
+                        if file_type.is_dir() {
+                            let new_path = &entry.path();
+                            if let Some(np) = new_path.to_str() {
+                                async_builder(Some(np)).await
+                            }
                         }
                     }
-                }
-                let path = entry.path();
-                if let Some(ext) = Path::new(&path).extension() {
-                    match ext.to_str() {
-                        Some(".html") | Some("html") => builder::html::html(&path, &entry).await.unwrap(),
-                        Some(".md") | Some("md") => builder::markdown::markdown(&path, &entry).await.unwrap(),
-                        _ => (),
+                    let path = entry.path();
+                    if let Some(ext) = Path::new(&path).extension() {
+                        match ext.to_str() {
+                            Some(".html") | Some("html") => {
+                                builder::html::html(&path, &entry).await.unwrap()
+                            }
+                            Some(".md") | Some("md") => {
+                                builder::markdown::markdown(&path, &entry).await.unwrap()
+                            }
+                            _ => (),
+                        }
                     }
                 }
             }
         }
     }
+    .boxed()
 }
